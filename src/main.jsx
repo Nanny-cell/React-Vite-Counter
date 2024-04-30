0080a19def88b5d316af9c9c9151dbdcf53e6e59
Nanni <danitza.p.96@gmail.com>
2024-04-30 18:12:43 -0400

    Se crea la aplicación CounterApp

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+//jsx = react + javascript
+//import { HelloWorldApp } from './HelloWorldApp'
+//import {FirstApp} from './FirstApp'
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import {CounterApp} from './CounterApp'
+
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root') ).render(
+    <React.StrictMode>
+        <CounterApp value={ 0 }/>
+    </React.StrictMode>
+);
